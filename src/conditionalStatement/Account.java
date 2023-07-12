86421d31b4b1e0dffb223281533710cdc947002e
athirarao <athira.csr@gmail.com>
2023-07-12 13:46:56 -0400

    Modified conditional statement

--- src/conditionalStatement/Account.java
+++ src/conditionalStatement/Account.java
@@ -1,48 +1,38 @@
 package conditionalStatement;
 
 public class Account {
-	
+
 	double balanceAmount = 5055.35;
 	int withdrawAmount;
 	int overdraftAmount;
 	int pinNumber;
 	int enteredPinnumber;
-	
-	
+
 	void atmwithdrawal() {
-		
-		if ( balanceAmount>withdrawAmount|| overdraftAmount>withdrawAmount) {
+
+		if (balanceAmount > withdrawAmount || overdraftAmount > withdrawAmount) {
 			System.out.println(" Customer withdraw cash");
-		}else {
-			
+		} else {
+
 			System.out.println("Insufficient fund");
-			
+
 		}
-	
-	
-		
+
 	}
-				
-		
+
 	void validatepinandWithdrawcash() {
-		if ( enteredPinnumber == pinNumber) {
+		if (enteredPinnumber == pinNumber) {
 			System.out.println("Pin is correct");
-			if (balanceAmount>withdrawAmount|| overdraftAmount>withdrawAmount) {
+			if (balanceAmount > withdrawAmount || overdraftAmount > withdrawAmount) {
 				System.out.println(" Customer withdraw cash");
-			}else {
+			} else {
 				System.out.println("No Balance");
 			}
-				
-			}else {
-			 System.out.println(" Incorrect Pin");
-			}
-			
+
+		} else {
+			System.out.println(" Incorrect Pin");
 		}
-		
-	
-		
+
 	}
-	
-	
 
-
+}
--- src/conditionalStatement/AtmMain.java
+++ src/conditionalStatement/AtmMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AtmMain {
 
 	public static void main(String[] args) {
-		
+		// New Account
 		Account athiraAccount = new Account();
 		
 		Scanner sc = new Scanner(System.in);
