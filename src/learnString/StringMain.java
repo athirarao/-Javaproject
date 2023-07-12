a1e5336f6fccabc8c7ddd69e106c6ba4221b856f
athirarao <athira.csr@gmail.com>
2023-07-12 11:03:56 -0400

    Modified String class

--- src/learnString/StringMain.java
+++ src/learnString/StringMain.java
@@ -1,12 +1,17 @@
 package learnString;
 
+import java.util.Scanner;
+
 public class StringMain {
 
 	public static void main(String[] args) {
 		
 		StringExample name = new StringExample();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name");
+		name.trainerName = sc.nextLine();
 		
-		name.trainerName = "Athira";
+		
 		name.displayTrainername();
 		  
 
